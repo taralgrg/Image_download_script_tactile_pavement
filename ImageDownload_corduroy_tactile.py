159694912476c199a333fab672b4908e204768e5
Taral Gurung <txg818@student.bham.ac.uk>
2019-07-04 16:11:59 +0100

    second commit

--- ImageDownload_corduroy_tactile.py
+++ ImageDownload_corduroy_tactile.py
@@ -2,7 +2,7 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"Honey Bottle,honey,honey squeeze","limit":111,"chromedriver":"/usr/local/bin/chromedriver","print_urls":True}   #creating list of arguments
+arguments = {"keywords":"Corduroy Hazard Warning Surface,corduroy hazard tactile paving,corduroy tactile paving slabs","limit":1000,"chromedriver":"/usr/local/bin/chromedriver","print_urls":True}   #creating list of arguments
 #Make sure to set chromedrive is the appropriate director if you want to download more than 100 images
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
